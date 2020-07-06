66bd34f79d32de1f0d9d6a4ab7492e895eefcd8e
huskarit99 <nguyenthaihocxm1999@gmail.com>
2020-07-06 20:37:50 +0700

    Change version from junit5 to junit4

---
+++ cicdjava/src/main/java/com/mycompany/app/Calculator.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        int rs = a + b;
+        return rs;
+    }
+    public int sub(int a, int b) {
+        int rs = a - b;
+        return rs;
+    }
+    public int mul(int a, int b) {
+        int rs = a * b;
+        return rs;
+    }
+    public int div(int a, int b) {
+        int rs = a / b;
+        return rs;
+    }
+    
+    public static void main(String[] args) {
+        // Calculator cal = new Calculator();
+        // System.out.println(cal.add(1, 2));
+        // System.out.println(cal.sub(5, 1));
+        // System.out.println(cal.mul(4, 10));
+        // System.out.println(cal.div(6,3));
+        System.out.println("Hello world !!!");
+    }
+    
+}
--- cicdjava/src/test/java/com/mycompany/app/CalculatorTest.java
+++ cicdjava/src/test/java/com/mycompany/app/CalculatorTest.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.mycompany.app;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
 
 /**
- *
- * @author Admin
+ * Unit test for simple App.
  */
 public class CalculatorTest {
     Calculator cal;
@@ -22,19 +17,19 @@ public class CalculatorTest {
         cal = new Calculator();
     }
     
-    @BeforeAll
+    @BeforeClass
     public static void setUpClass() {
     }
     
-    @AfterAll
+    @AfterClass
     public static void tearDownClass() {
     }
     
-    @BeforeEach
+    @Before
     public void setUp() {
     }
     
-    @AfterEach
+    @After
     public void tearDown() {
     }
 
